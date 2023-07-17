81e3e9fa8b060937ea7fb79f21dd1ea5cd20feb7
liuqingtong <liuqingtong@corp.netease.com>
2023-07-17 21:08:54 +0800

    feat: 添加 dagre 自适应布局

--- src/components/workflow/X6GraphIntergration.ts
+++ src/components/workflow/X6GraphIntergration.ts
@@ -1,8 +1,10 @@
 import { ref, defineComponent, onMounted } from "vue";
 import { register } from "@antv/x6-vue-shape";
-import DagNode from "@/views/test/DagNode1.vue";
-import { Graph, Path } from "@antv/x6";
+import dagre from '@dagrejs/dagre'
+import DagNode from "@/components/workflow/JobNode.vue";
+import { Edge, Graph, Path, Node } from "@antv/x6";
 import { Selection } from "@antv/x6-plugin-selection";
+import { debug } from "console";
 
 export function useX6Graph(graphContainerId: string) {
     console.log('x6 容器 ID ' + graphContainerId);
@@ -168,4 +170,79 @@ export function useX6Graph(graphContainerId: string) {
         x6Graph
     }
 
+}
+
+
+
+/**
+ * 自适应布局
+ * @param graph x6 画布
+ * @param dir 布局方向，取值有四个：LR RL TB BT
+ */
+export function autoLayout(graph: Graph, dir: string) {
+    
+
+    // 初始化 dagre 画布
+    const g = new dagre.graphlib.Graph();
+    g.setGraph({ 
+        rankdir: dir
+    });
+    g.setDefaultEdgeLabel(() => ({}));
+
+    // 设置节点到 dagre 画布
+    const nodes = graph.getNodes()
+    nodes.forEach(node => g.setNode(node.id, node.size()));
+
+    // 设置边到 dagre 画布
+    const edges: Edge[] = graph.getEdges()
+    edges.forEach((edge) => {
+        const source = edge.source as Edge.TerminalCellLooseData
+        const target = edge.target as Edge.TerminalCellLooseData
+        g.setEdge(source.cell as string, target.cell as string)
+    });
+
+    // 计算布局
+    dagre.layout(g);
+
+    // 更新 X6 图形位置和大小
+    g.nodes().forEach((id) => {
+        const node = graph.getCellById(id) as Node;
+        if (node) {
+            const pos = g.node(id)
+            node.position(pos.x, pos.y)
+        }
+    });
+  
+    // edges.forEach((edge) => {
+    //     const source = edge.getSourceNode()!
+    //     const target = edge.getTargetNode()!
+    //     const sourceBBox = source.getBBox()
+    //     const targetBBox = target.getBBox()
+
+    //     if ((dir === 'LR' || dir === 'RL') && sourceBBox.y !== targetBBox.y) {
+    //         const gap = dir === 'LR'
+    //             ? targetBBox.x - sourceBBox.x - sourceBBox.width
+    //             : -sourceBBox.x + targetBBox.x + targetBBox.width
+    //         const fix = dir === 'LR' ? sourceBBox.width : 0
+    //         const x = sourceBBox.x + fix + gap / 2
+    //         edge.setVertices([
+    //             { x, y: sourceBBox.center.y },
+    //             { x, y: targetBBox.center.y },
+    //         ])
+    //     } else if ((dir === 'TB' || dir === 'BT') && sourceBBox.x !== targetBBox.x) {
+    //         const gap = dir === 'TB'
+    //             ? targetBBox.y - sourceBBox.y - sourceBBox.height
+    //             : -sourceBBox.y + targetBBox.y + targetBBox.height
+    //         const fix = dir === 'TB' ? sourceBBox.height : 0
+    //         const y = sourceBBox.y + fix + gap / 2
+    //         edge.setVertices([
+    //             { x: sourceBBox.center.x, y },
+    //             { x: targetBBox.center.x, y },
+    //         ])
+    //     } else {
+    //         edge.setVertices([]);
+    //     }
+    // });
+
+    graph.resetCells([ ...nodes, ...edges ])
 }
